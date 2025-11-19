d25d50b10f721780e19a019601df4452ad766d9f
inochi <souchin.i.19@gmail.com>
2025-11-19 10:30:26 +0900

    change the code

--- sinsu_test.bash
+++ sinsu_test.bash
@@ -16,11 +16,11 @@ out=$(echo 5.5 | ./sinsu)
 [ "${out}" = 0b101 ] || ng "$LINENO"
 
 out=$(echo あ | ./sinsu)
-[ "$?" = 0 ]     || ng "$LINENO"
+[ "$?" = 1 ]     || ng "$LINENO"
 [ "${out}" = ""] || ng "$LINENO"
 
 out=$(echo | ./sinsu)
-[ "$?" = 0 ]      || ng "$LINENO"
+[ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = ""] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
