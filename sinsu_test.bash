3cd8315a6e3cafd7eb19da2bd0c48cd16bc7288c
inochi <souchin.i.19@gmail.com>
2025-11-19 09:32:48 +0900

    plus code

---
+++ sinsu_test.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2025 Sota Ino
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+       echo ${1}行目が違う
+       res=1
+}
+
+res=0
+
+out=$(echo 5 | ./sinsu)
+[ "${out}" = 0b101 ] || ng "$LINENO"
+
+out=$(echo 5.5 | ./sinsu)
+[ "${out}" = ob101 ] || ng "$LINENO"
+
+out=$(echo あ | ./sinsu)
+[ "$?" = 1 ]     || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
+
+out=$(echo | ./sinsu)
+[ "$?" = 1]      || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
